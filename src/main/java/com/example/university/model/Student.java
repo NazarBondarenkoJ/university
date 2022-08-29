60c6cfead500d23565695df6f80d58cb05c8c917
nbondarenko <nbondarenko@jappware.com>
2022-08-29 16:39:42 +0300

    Added caching for Student and Teacher entities. Added L2 Cache to an app.

--- src/main/java/com/example/university/model/Student.java
+++ src/main/java/com/example/university/model/Student.java
@@ -5,6 +5,7 @@ import com.example.university.model.courses.CourseRating;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -16,24 +17,32 @@ import java.util.Set;
 @Data
 @EqualsAndHashCode(exclude = {"ratings", "course"}, callSuper = false)
 @ToString(exclude = {"books", "ratings", "teacher", "course", "diploma", "courseWork"})
+@Cacheable
+@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class Student extends Person{
 
+    @org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
     @ManyToMany(cascade = CascadeType.ALL)
     private List<Book> books = new ArrayList<>();
 
+    @org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
     @OneToMany(mappedBy = "student", cascade = CascadeType.ALL)
     private Set<CourseRating> ratings = new HashSet<>();
 
+    @org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "teacher_student")
     private Teacher teacher;
 
+    @org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
     @ManyToOne(cascade = CascadeType.ALL)
     private Course course;
 
+    @org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
     @OneToOne(cascade = CascadeType.ALL)
     private Diploma diploma;
 
+    @org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
     @OneToOne(cascade = CascadeType.ALL)
     private CourseWork courseWork;
 }
